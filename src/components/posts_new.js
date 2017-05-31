f93b3dbd8f2d8fed58b1476fe4ca42ade6b74609
Jennifer Cen <stephen@hedgeable.com>
2017-05-31 17:48:00 -0400

    redux blog app forms

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,72 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+//Field knows how to communicate with redux-form (handles action create/event handlers); Doesn't know how to show itself on the screen
+//reduxForm is a function that is similar to the connect function in the react-redux
 
 class PostsNew extends Component {
+	
+	//field will call this function (within component) sometime in the future
+	renderField(field){
+		return (
+			<div className="form-group">
+				<label>{field.label}</label>
+				<input 
+					type="text" 
+					className="form-control"
+					{...field.input}
+				/>
+				{field.meta.error}
+			</div>
+		);
+	}
+
 	render() {
 		return (
-			<div>
-				Create new form
-			</div>
+			<form>
+				<Field
+					label="Title For Post"
+					name="title"
+					component={this.renderField} />
+				<Field
+					label="Categories"
+					name="categories"
+					component={this.renderField} />
+				<Field
+					label="Post Content"
+					name="content"
+					component={this.renderField} />
+			</form>
 		)
 	}
 }
 
-export default PostsNew;
+function validate(values) {
+	//console.log('values', values) //=> {title: 'adsdf', ...}
+	const errors = {};
+
+	//Validate the inputs from 'values'
+	if(!values.title){
+		errors.title = "Enter a title!";
+	}
+
+	if (!values.categories){
+		errors.content = "Enter some categories";
+	}
+
+	if(!values.content){
+		errors.content = "Enter some content";
+	}
+
+	//if errors is empty, redux form assumes nothing is wrong with our form
+	//if errors has *any* properties, redux form assumes form is invalid
+	return errors;
+}
+
+//Field Props: 1.) name (name of state being produced), 2.) component: function that returns some amount of JSX
+
+export default reduxForm({ //allow us to communicate directly from component to reducer of state
+	form: 'PostsNewForm', //should be unique: name of form => ensure if we show multiple diff forms at the same time, redux form will handle all the diff state correctly; live in isolation and not share state with any other form
+	validate //validate the form is called automatically
+})(PostsNew);
+//Note: if another file has the same form name (that form state will be merged together to this form state: Make sure string of form name is unique)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer_posts';
+import { reducer as formReducer } from 'redux-form'; //renaming the reducer to alias formReducer
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
+
+//import reducer inside the redux form library and hook it up to our combineReducers call - redux form uses our redux instance/our instance of the redux store (for handling all the state that is being produced by the form) 
+//benefit of redux form: save us from wiring up a bunch of action creators
 
 export default rootReducer;
